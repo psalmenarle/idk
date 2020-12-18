7ecdbdc2078ec01f949e6fcf67073e3ef6ce39d4
psalmenarle <60122554+psalmenarle@users.noreply.github.com>
2020-12-18 22:19:29 +0800

    Update canvas.php

--- canvas.php
+++ canvas.php
@@ -1,5 +1,5 @@
 <h1> Canvas </h1>
-<a class="link" href="7.1/index.php">Learning Task 7.1</a>
-<a class="link" href="7.1/index.php">Learning Task 7.1</a>
-<a class="link" href="7.1/index.php">Learning Task 7.1</a>
-<a class="link" href="7.1/index.php">Learning Task 7.1</a>
+<a class="link hover" href="7.1/index.php">Learning Task 7.1</a>
+<a class="link hover" href="7.1/index.php">Learning Task 7.1</a>
+<a class="link hover" href="7.1/index.php">Learning Task 7.1</a>
+<a class="link hover" href="7.1/index.php">Learning Task 7.1</a>
