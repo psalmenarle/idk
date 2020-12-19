ffb09c4fb97764ab9880607ee5a7f42a89d536ab
psalmenarle <60122554+psalmenarle@users.noreply.github.com>
2020-12-19 17:04:30 +0800

    Add files via upload

---
+++ about.php
@@ -0,0 +1,14 @@
+<div class="container">
+    <h1>  Psalm Enarle </h1> 
+<table class="can">
+  <tr>
+    <td><img  src="css/about.svg" width="200px" height="200px"/></td>
+  </tr>
+  <tr>
+    <td>Psalm Enarle is currently a 3rd year year Information Technology Student 
+        in University of St. La Salle. Studying about Web Development, Animation and alike 
+        and trying to improve and learn more about Web Development.
+    </td>
+  </tr>
+</table>
+</div>
