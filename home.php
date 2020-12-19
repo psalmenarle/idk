a7aab50844dbcec3550c6b921b47ac07c31e99e5
psalmenarle <60122554+psalmenarle@users.noreply.github.com>
2020-12-19 15:39:35 +0800

    Add files via upload

---
+++ home.php
@@ -0,0 +1,8 @@
+<div class="container">
+    <h1>  CANVAS ACTIVITIES </h1>
+<table class="can">
+  <tr>
+    <td><img  src="css/sun.svg" width="200px" height="200px"/> </td>
+  </tr>
+</table>
+</div>
