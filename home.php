68117afb156e0f7ec466aed0192d20e8394473e4
psalmenarle <60122554+psalmenarle@users.noreply.github.com>
2020-12-19 16:08:09 +0800

    Update home.php

--- home.php
+++ home.php
@@ -1,4 +1,4 @@
-<div class="container">
+<div class="container" style="display:inline-block;">
     <h1> PSALM's ITL303 PORTFOLIO </h1>
     <img  src="css/sun.svg" width="300px" height="300px"/>
 <table class="can">
