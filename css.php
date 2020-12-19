7e871b76b0a427bde1f9a158157ea55cd8af6009
psalmenarle <60122554+psalmenarle@users.noreply.github.com>
2020-12-19 13:49:35 +0800

    Add files via upload

---
+++ css.php
@@ -0,0 +1,20 @@
+<div class="container">
+    <h1> CSS ACTIVITIES </h1>
+<table class="css">
+  <tr>
+    <td><a href="https://codepen.io/psalmenarle/pen/Yzqxgwv">Learning Task 2.2 ROTATE</a></td>
+    <td><a href="https://codepen.io/psalmenarle/pen/OJNjqRG">Learning Task 2.2 SCALE</a></td>
+    <td><a href="https://codepen.io/psalmenarle/pen/ZEWJPdX">Learning Task 2.2 SKEW</a></td>
+  </tr>
+  <tr>
+    <td><a href="https://codepen.io/psalmenarle/pen/OJNjGLy">Learning Task 2.2 TRANSLATE</a></td>
+    <td><a href="2.3/index.php">Learning Task 2.3 </a></td>
+    <td><a href="https://codepen.io/psalmenarle/pen/xxVYoJw">Learning Task 3.2 TRANSITION</a></td>
+  </tr>
+  <tr>
+    <td><a href="https://codepen.io/psalmenarle/pen/JjXpQqR">Learning Task 3.3 ANIMATION</a></td>
+    <td><a href="https://codepen.io/psalmenarle/pen/rNeZXER">Learning Task 4.1</a></td>
+    <td><a href="https://cle-prelim.herokuapp.com/">CLE</a></td>
+  </tr>
+</table>
+</div>
---
+++ svg.php
@@ -0,0 +1,19 @@
+<div class="container">
+    <h1>  SVG ACTIVITIES </h1>
+<table class="svg">
+  <tr>
+    <td><a href = "5.1/index.php">Learning Task 5.1</a></td>
+  </tr>
+  <tr>
+    <td><a href = "6.1/index.php">Learning Task 6.1</a></td>
+    
+  </tr>
+  <tr>
+    <td><a href = "6.2/index.php">Learning Task 6.2</a></td>
+  </tr>
+  <tr>
+    <td><a href = "midcle/index.php">CLE </a></td>
+    
+  </tr>
+</table>
+</div>
