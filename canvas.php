b9714f34cb0d6df99324031c08aebcd54aaa58e4
psalmenarle <60122554+psalmenarle@users.noreply.github.com>
2020-12-19 10:36:31 +0800

    Update canvas.php

--- canvas.php
+++ canvas.php
@@ -1,16 +1,30 @@
-<h1> Canvas </h1>
+<h1> CSS ACTIVITIES </h1>
 <div class="container">
     <div class="link-wrapper">
-<a class="link hover" href="7.1/index.php">Learning Task 7.1</a>
+<a href="https://codepen.io/psalmenarle/pen/Yzqxgwv">Learning Task 2.2 ROTATE</a>
   </div>
 <div class="link-wrapper">
-<a class="link hover" href="7.1/index.php">Learning Task 7.1</a>
+<a href="https://codepen.io/psalmenarle/pen/OJNjqRG">Learning Task 2.2 SCALE</a>
   </div>
 <div class="link-wrapper">
-<a class="link hover" href="7.1/index.php">Learning Task 7.1</a>
+<a href="https://codepen.io/psalmenarle/pen/ZEWJPdX">Learning Task 2.2 SKEW</a>
   </div>
 <div class="link-wrapper">
-<a class="link hover" href="7.1/index.php">Learning Task 7.1</a>
+<a href="https://codepen.io/psalmenarle/pen/OJNjGLy">Learning Task 2.2 TRANSLATE</a>
+  </div>
+  <div class="link-wrapper">
+<a href="2.3/index.php">Learning Task 2.3 </a>
+  </div>
+  <div class="link-wrapper">
+<a href="https://codepen.io/psalmenarle/pen/xxVYoJw">Learning Task 3.2 TRANSITION</a>
+  </div>
+  <div class="link-wrapper">
+<a href="https://codepen.io/psalmenarle/pen/JjXpQqR">Learning Task 3.3 ANIMATION</a>
+  </div>
+  <div class="link-wrapper">
+<a href="https://codepen.io/psalmenarle/pen/rNeZXER">Learning Task 4.1</a>
+  </div>
+  <div class="link-wrapper">
+<a href="https://cle-prelim.herokuapp.com/">CLE</a>
   </div>
 </div>
-  
