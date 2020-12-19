cc11edfe53e0744b61cd1a623cb813a45d98c7c9
psalmenarle <60122554+psalmenarle@users.noreply.github.com>
2020-12-19 16:02:08 +0800

    Update home.php

--- home.php
+++ home.php
@@ -1,6 +1,6 @@
 <div class="container">
-    <h1>  CANVAS ACTIVITIES </h1>
-    <img  src="css/sun.svg" width="200px" height="200px"/>
+    <h1> PSALM's ITL303 PORTFOLIO </h1>
+    <img  src="css/sun.svg" width="300px" height="300px"/>
 <table class="can">
   <tr>
     <td> </td>
