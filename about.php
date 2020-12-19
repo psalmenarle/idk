08df395bcb58365007d6408ef6c1d2c2e942769a
psalmenarle <60122554+psalmenarle@users.noreply.github.com>
2020-12-19 17:09:01 +0800

    Update about.php

--- about.php
+++ about.php
@@ -1,10 +1,15 @@
 <div class="container">
     <h1>  Psalm Enarle </h1> 
-    <img  src="css/about.svg" width="400px" height="400px"/>
+    <img  src="css/about.svg" width="400px" height="300px"/>
 <table class="can">
   <tr>
-    <td>Psalm Enarle is currently a 3rd year year Information Technology Student 
-        in University of St. La Salle.&nbsp Studying about Web Development, Animation and alike 
+    <td class="me">Psalm Enarle is currently a 3rd year year Information Technology Student 
+        in University of St. La Salle.
+    </td>
+  </tr>
+     <tr>
+    <td class="me">
+        Studying about Web Development, Animation and alike 
         and trying to improve and learn more about Web Development.
     </td>
   </tr>
