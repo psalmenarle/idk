77394fda305590813f295703cbd1fda8dcf4c471
psalmenarle <60122554+psalmenarle@users.noreply.github.com>
2020-12-19 16:39:41 +0800

    Update home.php

--- home.php
+++ home.php
@@ -1,9 +1,4 @@
 <div class="container">
     <h1 class="home"> WELCOME TO PSALM's ITL303 PORTFOLIO </h1>
     <img  src="midcle/daisy.svg" width="300px" height="300px"/>
-<table class="can">
-  <tr>
-    <td> </td>
-  </tr>
-</table>
 </div>
