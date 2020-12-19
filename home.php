01f5a228a7bfe810e9718a36450b5bfba92c1b53
psalmenarle <60122554+psalmenarle@users.noreply.github.com>
2020-12-19 15:55:25 +0800

    Update home.php

--- home.php
+++ home.php
@@ -1,8 +1,9 @@
 <div class="container">
     <h1>  CANVAS ACTIVITIES </h1>
+    <img  src="css/sun.svg" width="200px" height="200px"/>
 <table class="can">
   <tr>
-    <td><img  src="css/sun.svg" width="200px" height="200px"/> </td>
+    <td> </td>
   </tr>
 </table>
 </div>
